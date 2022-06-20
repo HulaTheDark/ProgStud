e9498a560034133bbbcc94177508c17f63f26172
Michael Larsen <michaelrichs@live.dk>
2022-06-20 12:50:58 +0200

    Update constant.php

--- php/constant.php
+++ php/constant.php
@@ -1,5 +1,5 @@
 <?php
 //reCAPTCHA Configuration - go to Google and get the below keys http://www.google.com/recaptcha/admin
-define('6Le1T4UgAAAAAKF3AI_9cnTPTcdkSM4Sy40fJBNK',"6Le1T4UgAAAAAKF3AI_9cnTPTcdkSM4Sy40fJBNK"); 
-define('6Le1T4UgAAAAAPRHVVNfeCi_KWzq2QlzqmmEr1op',"6Le1T4UgAAAAAPRHVVNfeCi_KWzq2QlzqmmEr1op")
+define("6Le1T4UgAAAAAKF3AI_9cnTPTcdkSM4Sy40fJBNK"); 
+define("6Le1T4UgAAAAAPRHVVNfeCi_KWzq2QlzqmmEr1op")
 ?>
