cac1a2953e0510a9d2d25600fe5785c4fa6dee67
Michael Larsen <michaelrichs@live.dk>
2022-06-20 12:14:42 +0200

    reCAPTCHA

---
+++ php/constant.php
@@ -0,0 +1,5 @@
+<?php
+//reCAPTCHA Configuration - go to Google and get the below keys http://www.google.com/recaptcha/admin
+define('6Ld_LoUgAAAAANrcVcsDjLcM_UFXT-oUSWY8KdAC',"6Ld_LoUgAAAAANrcVcsDjLcM_UFXT-oUSWY8KdAC"); 
+define('6Ld_LoUgAAAAAPn-S1N_LX_oh4Xd-_ov5D-XsuAa',"6Ld_LoUgAAAAAPn-S1N_LX_oh4Xd-_ov5D-XsuAa");
+?>
