690a8849a39e5f3263145635c9a7bd4508246443
Michael Larsen <michaelrichs@live.dk>
2022-06-20 12:37:56 +0200

    Update

--- php/constant.php
+++ php/constant.php
@@ -1,5 +1,5 @@
 <?php
 //reCAPTCHA Configuration - go to Google and get the below keys http://www.google.com/recaptcha/admin
-define('6Ld_LoUgAAAAANrcVcsDjLcM_UFXT-oUSWY8KdAC',"6Ld_LoUgAAAAANrcVcsDjLcM_UFXT-oUSWY8KdAC"); 
-define('6Ld_LoUgAAAAAPn-S1N_LX_oh4Xd-_ov5D-XsuAa',"6Ld_LoUgAAAAAPn-S1N_LX_oh4Xd-_ov5D-XsuAa")
+define('6Le1T4UgAAAAAKF3AI_9cnTPTcdkSM4Sy40fJBNK',"6Le1T4UgAAAAAKF3AI_9cnTPTcdkSM4Sy40fJBNK"); 
+define('6Le1T4UgAAAAAPRHVVNfeCi_KWzq2QlzqmmEr1op',"6Le1T4UgAAAAAPRHVVNfeCi_KWzq2QlzqmmEr1op")
 ?>
